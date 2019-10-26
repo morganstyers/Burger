a1e4946d7a56d5038af11376ff4c2680b7cdf0c3
Morgan Styers <morganstyers@gmail.com>
2019-10-26 17:04:15 -0400

    'getting somewhere'

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (name) VALUES ('Blackbean Burger');
+INSERT INTO burgers (name) VALUES ("Bobbi's Burger");
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name, devoured) VALUES ('Good Burger', true);
