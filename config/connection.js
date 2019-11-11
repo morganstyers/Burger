283060513d0597fba4d634b29ebc8495d2726850
Morgan Styers <morganstyers@gmail.com>
2019-11-11 17:45:22 -0500

    heroki

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,18 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 
 // Make connection.
 connection.connect(function(err) {
